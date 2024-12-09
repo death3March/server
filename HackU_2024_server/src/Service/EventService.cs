ceefb98278a07f3cdadd55e1d10005d637c765db
ukai <73654679+snct-ukai@users.noreply.github.com>
2024-12-10 00:30:17 +0900

    update: send user info when user joined room

--- HackU_2024_server/src/Service/EventService.cs
+++ HackU_2024_server/src/Service/EventService.cs
@@ -10,7 +10,9 @@ public static class EventService
     public static async UniTask OnReceiveAsync(Client client, ClientMessage data)
     {
         Console.WriteLine(nameof(data.TypeCase));
-        ServerMessage? res = null;
+        var clients = DataBaseManager.GetClients(client.RoomName);
+        
+        ServerMessage[]? res = null;
         switch (data.TypeCase)
         {
             case ClientMessage.TypeOneofCase.None:
@@ -35,11 +37,22 @@ public static class EventService
             default:
                 throw new ArgumentOutOfRangeException(nameof(data));
         }
+
+        if (res is not null)
+        {
+            foreach (var r in res)
+            {
+                foreach (var c in clients)
+                {
+                    c.SendAsync(r.ToByteArray()).Forget();
+                }
+            }
+        }
     }
 
-    private static async UniTask<ServerMessage?> OnRoomJoinRequest(Client client, RoomJoinRequest req)
+    private static async UniTask<ServerMessage[]?> OnRoomJoinRequest(Client client, RoomJoinRequest req)
     {
-        return await UniTask.Run<ServerMessage?>(() => { 
+        return await UniTask.Run<ServerMessage[]?>(() => { 
             var roomName = req.Data.RoomCode;
             var room = DataBaseManager.GetRoom(roomName);
             if (room is null)
@@ -68,15 +81,50 @@ public static class EventService
             }
             
             client.RoomName = roomName;
+            client.Nickname = req.Data.Nickname;
             DataBaseManager.UpdateClientData(client);
             Console.WriteLine("Room Joined");
-            return null;
+            var res = new ServerMessage
+            {
+                RoomJoinResponse = new RoomJoinResponse
+                {
+                    Data = new RoomJoinResponse.Types.Data
+                    {
+                        RoomCode = roomName,
+                        PlayerId = client.UserID,
+                        Nickname = client.Nickname
+                    }
+                }
+            };
+
+            UniTask.Run(() =>
+            {
+                var alreadyJoinedClients = DataBaseManager.GetClients(roomName).Where(c => c.GlobalUserId != client.GlobalUserId);
+                foreach (var c in alreadyJoinedClients)
+                {
+                    var res2 = new ServerMessage
+                    {
+                        RoomJoinResponse = new RoomJoinResponse
+                        {
+                            Data = new RoomJoinResponse.Types.Data
+                            {
+                                RoomCode = roomName,
+                                PlayerId = c.UserID,
+                                Nickname = c.Nickname
+                            }
+                        }
+                    };
+                    client.SendAsync(res2.ToByteArray()).Forget();
+                }
+            });
+            
+            return [res];
         });
     }
     
-    private static async UniTask<ServerMessage?> OnRoomLeaveRequest(Client client, RoomLeaveRequest req)
+    private static async UniTask<ServerMessage[]?> OnRoomLeaveRequest(Client client, RoomLeaveRequest req)
     {
-        return await UniTask.Run<ServerMessage?>(() => { 
+        return await UniTask.Run<ServerMessage[]?>(() => { 
             var room = DataBaseManager.GetRoom(client.RoomName);
             if (room is null)
                 return null;
