5e062986f2281120e448283d8bf1096a10e23f39
ukai <73654679+snct-ukai@users.noreply.github.com>
2024-12-13 22:04:39 +0900

    update: add healthCheck

--- HackU_2024_server/src/Server/Server.cs
+++ HackU_2024_server/src/Server/Server.cs
@@ -1,6 +1,7 @@
 ﻿using System.Net;
 using System.Net.Sockets;
 using System.Net.WebSockets;
+using System.Text;
 using Cysharp.Threading.Tasks;
 using HackU_2024_server.DataBase;
 using HackU_2024_server.Service;
@@ -11,6 +12,7 @@ public static class Server
 {
     private static bool _isRunning;
     private static readonly TcpListener TcpListener = new(IPAddress.Any, 8080);
+    private static readonly TcpListener HealthCheckTcpListener = new(IPAddress.Any, 8081);
 
     public static async UniTask Start()
     {
@@ -19,6 +21,9 @@ public static class Server
         _isRunning = true;
 
         TcpListener.Start();
+        HealthCheckTcpListener.Start();
+
+        HealthCheckHandler().Forget();
 
         while (_isRunning)
         {
@@ -39,6 +44,30 @@ public static class Server
             client.Socket = null;
             DataBaseManager.UpdateClientData(client);
         });
+    }
+    private static async UniTask HealthCheckHandler()
+    {
+        while (_isRunning)
+        {
+            var healthCheckClient = await HealthCheckTcpListener.AcceptTcpClientAsync();
+            await using var stream = healthCheckClient.GetStream();
+            var buffer = new byte[1024];
+            var bytesRead = await stream.ReadAsync(buffer);
+            var request = Encoding.UTF8.GetString(buffer, 0, bytesRead);
+
+            if (request.StartsWith("GET"))
+            {
+                var response = "HTTP/1.1 200 OK\r\nContent-Length: 2\r\n\r\nOK"u8.ToArray();
+                await stream.WriteAsync(response);
+            }
+            else
+            {
+                var response = "HTTP/1.1 404 Not Found\r\nContent-Length: 0\r\n\r\n"u8.ToArray();
+                await stream.WriteAsync(response);
+            }
+
+            healthCheckClient.Close();
+        }
     }
 
     private static async UniTask ClientHandler(Client client, byte[] data)
